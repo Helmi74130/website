2868f6f1113c036b467c61bb6470d183862dfb28
Helmi74130 <88458882+Helmi74130@users.noreply.github.com>
2023-01-09 09:26:26 +0100

    add animation text and img for demarqued section

--- js/script.js
+++ js/script.js
@@ -25,3 +25,44 @@ let splide = new Splide( '.splide', {
     arrowPath: 'none',
     perPage: 2,
   }  ).mount();
+
+
+// Wrap every letter in a span
+let textWrapper = document.querySelector('.ml7 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+function animeText(){
+  anime.timeline({loop: false})
+  .add({
+    targets: '.ml7 .letter',
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 2000,
+    easing: "easeOutExpo",
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml7',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 3000
+  });
+}
+
+  let titleDemarqued = document.querySelector('#titleDemarqued');
+  
+  function observeImg() {
+    const observerLastImg = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          animeText()
+        }
+    }, {
+        threshold: 0.99
+    });
+
+    observerLastImg.observe(titleDemarqued);
+};
+
+observeImg();
