511deb71bcacddd251ad94a7545d995e494359fc
Helmi74130 <88458882+Helmi74130@users.noreply.github.com>
2023-01-12 09:44:57 +0100

    complete responsive page

--- js/script.js
+++ js/script.js
@@ -20,12 +20,31 @@ let splide = new Splide( '.splide', {
   splide.mount();
 
 
-  new Splide( '#slider2', {
-    type   : 'loop',
-    autoplay: 'true',
-    arrowPath: 'none',
-    perPage: 2,
-  }  ).mount();
+  
+
+
+
+  function myFunction(x) {
+    if (x.matches) { // If media query matches
+      new Splide( '#slider2', {
+        type   : 'loop',
+        autoplay: 'true',
+        arrowPath: 'none',
+        perPage: 1,
+      }  ).mount();
+    } else {
+      new Splide( '#slider2', {
+        type   : 'loop',
+        autoplay: 'true',
+        arrowPath: 'none',
+        perPage: 2,
+      }  ).mount();
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 992px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
 
 
 
